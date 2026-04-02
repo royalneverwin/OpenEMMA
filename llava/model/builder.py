2424a010698c797c45d44b1736b9ded508d28db4
royalneverwin <2301213100@pku.edu.cn>
2026-04-02 16:48:45 +0800

    fix bugs for load in 4 bit

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -91,7 +91,6 @@ def load_pretrained_model(
     if load_8bit:
         kwargs["load_in_8bit"] = True
     elif load_4bit:
-        kwargs["load_in_4bit"] = True
         kwargs["quantization_config"] = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.float16,
